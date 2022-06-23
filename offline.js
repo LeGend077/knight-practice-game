ab8c3a9fe87e2f8780e57e7b0a8c665fd292e4c6
LeGend <98607285+LeGend077@users.noreply.github.com>
2022-06-23 17:34:53 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1655985710,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/knight-sheet0.png",
+		"images/knight-sheet1.png",
+		"images/knight-sheet2.png",
+		"images/bg.png",
+		"images/tiledbackground.png",
+		"images/platform1-sheet0.png",
+		"images/platform2-sheet0.png",
+		"images/platform3-sheet0.png",
+		"images/platform4-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/heart-sheet0.png",
+		"images/void-sheet0.png",
+		"images/win_platform-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
